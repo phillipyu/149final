78ba9a217ba5755292d0c8b49e1ff4c7e15af5bc
phillipyu <phillip.yu.1@gmail.com>
2017-04-30 20:08:25 -0400

    added classification tree model

--- finalproject.R
+++ finalproject.R
@@ -2,6 +2,9 @@
 library(mgcv) # for gam
 library(rpart) # for rpart
 library(randomForest) # for randomForest
+library(aod) #for betabin function
+library(rpart) # for rpart
+
 
 test = read.csv(file.choose())
 train = read.csv(file.choose())
@@ -104,4 +107,21 @@ train.rf.na = randomForest(voted ~ gender + cd + hd + age + dbdistance + vccdist
 predict.rf = predict(train.rf.na, test.na, type="prob")
 
 write.csv(predict.rf, file = "rf.csv")
+
+# determining mtry; this code takes forever to run so doesn't really work
+#cv.rf = rfcv(na.roughfix(train.na[,2:21]),train.na$voted,step=0.9)
+#cbind(nvars = cv.rf$n.var, error.rate = cv.rf$error.cv)
+
+# fitting classification tree
+ctree = rpart(voted ~ gender + cd + hd + age + dbdistance + vccdistance + party + 
+                racename + hsonly + mrrg + chldprsnt + cath + evang + nonchrst + 
+                otherchrst + days.since.reg + dbdistance.na + 
+                vccdistance.na,data=train.na,method="class",
+              parms=list(split="information"),cp=0.0001)
+printcp(ctree)
+plotcp(ctree)
+
+predict.ctree = predict(ctree,newdata=test.na,type="prob")
+
+write.csv(predict.ctree, file="ctree.csv")
 
